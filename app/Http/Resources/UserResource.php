ce45c885b29ce39b0d40a9cad3e9e029c6774090
unknown <murithipaul1@gmail.com>
2020-06-17 13:12:24 -0700

    updated user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->name,
             'email'=>$this->email,
+            'roles'=>new RoleCollection($this->roles),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
