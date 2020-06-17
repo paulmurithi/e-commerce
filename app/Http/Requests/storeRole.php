db4c2f4831a4ca603ec49bd335896abbd8f08128
unknown <murithipaul1@gmail.com>
2020-06-17 10:33:43 -0700

    updated validation logic

--- app/Http/Requests/storeRole.php
+++ app/Http/Requests/storeRole.php
@@ -13,7 +13,7 @@ class storeRole extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/storeUser.php
+++ app/Http/Requests/storeUser.php
@@ -13,7 +13,7 @@ class storeUser extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
