8a80243a0af4e8a96094d11378c409c59108a417
unknown <murithipaul1@gmail.com>
2020-06-17 08:06:57 -0700

    added form validation rules

--- app/Http/Requests/storePermission.php
+++ app/Http/Requests/storePermission.php
@@ -24,7 +24,7 @@ class storePermission extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>'required|string|unique:roles'
         ];
     }
 }
--- app/Http/Requests/storeRole.php
+++ app/Http/Requests/storeRole.php
@@ -24,7 +24,7 @@ class storeRole extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>'required|string|unique:roles'
         ];
     }
 }
