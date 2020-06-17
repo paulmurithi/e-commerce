bae891808959b767ec0135b029bc85ddce2e8139
unknown <murithipaul1@gmail.com>
2020-06-17 09:12:54 -0700

    updated user resource

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -12,6 +12,9 @@ class UserCollection extends ResourceCollection
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
+
+    public $collects = 'App\Http\Resources\UserResource';
+    
     public function toArray($request)
     {
         return parent::toArray($request);
