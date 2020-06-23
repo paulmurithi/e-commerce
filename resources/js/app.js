5f53d333b3d5b7e6fb191a21ef3dfc763171c664
unknown <murithipaul1@gmail.com>
2020-06-23 09:28:42 -0700

    updated app .js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,27 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import App from './App.vue';
 
-const app = new Vue({
-    el: '#app',
+import store  from './store';
+import VueRouter from 'vue-router';
+
+vue.use(VueRouter);
+
+import routes from './routes';
+
+const router = new VueRouter({
+    routes
 });
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+// const app = new Vue({
+//     el: '#app',
+// });
+
+new Vue({
+    store,
+    router,
+    render: h => h(App)
+  }).$mount("#app");
