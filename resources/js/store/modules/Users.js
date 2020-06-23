8d26e3633d8d2b72be34468f035b37a1cd2e2b5c
unknown <murithipaul1@gmail.com>
2020-06-23 13:39:14 -0700

    updated users module

--- resources/js/store/modules/Users.js
+++ resources/js/store/modules/Users.js
@@ -4,12 +4,41 @@ const state = {
     users:[],
 };
 const getters = {
-    allUsers(state){
+    allUsers:(state)=>{
         return state.users
     },
 };
-const actions = {};
-const mutations = {};
+const actions = {
+    async fetchUsers({commit}){
+        const response = await axios.get('api/users');
+        commit('SET_USER',response.data);
+    },
+    async  addUser({commit}, user){
+        const response = await axios.post('api/users', user);
+        commit('ADD_USER', response.data);
+    },
+    async editUser({commit}, updatedUser){
+        const response = await axios.put(`api/users/${updatedUser.id}`, updatedUser);
+        commit('EDIT_USER', response.data);
+    },
+    async deleteUser({commit}, id){
+        await axios.delete(`api/users/${id}`);
+        commit('DELETE_USER', id);
+    }
+};
+const mutations = {
+    SET_USERS:(state, users)=>state.users = users,
+
+    ADD_USER:(state, user)=>state.users.unshift(user),
+
+    EDIT_USER:(state, updatedUser)=>{
+        const index = state.users.findIndex(user=>user.id == updatedUser.id);
+        if(index!==-1){
+            state.users.splice(index, 1, updatedUser);
+        }
+    },
+    DELETE_USER:(state, id)=>state.users.filter(user=>user.id==id)
+};
 
 export default{
     state,
